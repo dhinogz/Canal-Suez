5bbfb316e40b6331dbfd1c8ad2c1909cf3c04d1b
David <davidhinojosauni@gmail.com>
2021-09-07 19:07:06 -0500

    Move functions to Functions.h file to have a cleaner main

---
+++ Functions.h
@@ -0,0 +1,45 @@
+void printVector(vector<registro> vec)
+{
+	for(int i = 0; i < vec.size(); i++)
+	{
+		cout << vec[i].fecha << " ";
+		cout << vec[i].hora << " ";
+		cout << vec[i].entrada << " ";
+		cout << vec[i].ubi << " ";
+		cout << vec[i].fechaInt << " ";
+		cout << vec[i].ubiStr << "\n";
+	}
+	cout << "\n";
+}
+
+void sortByUbi(vector<registro> &data, int &numComp)
+{
+	for(int i = 0; i < data.size() - 1; i++)
+	{
+		for (int j = i + 1; j < data.size(); j++)
+		{
+			numComp++;
+			if (data[i].ubiStr > data[j].ubiStr)
+			{
+				std::swap(data[i], data[j]);
+			}
+			else if (data[i].ubiStr == data[j].ubiStr)
+			{
+				if (data[i].fechaInt > data[j].fechaInt)
+				{
+					std::swap(data[i], data[j]);
+				}
+			}
+		}
+	}
+}
+
+string ubiToStart(string ubi)
+{
+	string ubiStart;
+	for (int i = 0; i < 3; i++)
+	{
+		ubiStart += ubi[i];
+	}
+	return ubiStart;
+}
--- main.cpp
+++ main.cpp
@@ -11,8 +11,6 @@ using std::string;
 using std::ifstream;
 using std::sort;
 
-#include "DateConverter.h"
-
 struct registro
 {
 	string fecha;
@@ -23,51 +21,8 @@ struct registro
 	string ubiStr;
 };
 
-void printVector(vector<registro> vec)
-{
-	for(int i = 0; i < vec.size(); i++)
-	{
-		cout << vec[i].fecha << " ";
-		cout << vec[i].hora << " ";
-		cout << vec[i].entrada << " ";
-		cout << vec[i].ubi << " ";
-		cout << vec[i].fechaInt << " ";
-		cout << vec[i].ubiStr << "\n";
-	}
-	cout << "\n";
-}
-
-void sortByUbi(vector<registro> &data, int &numComp)
-{
-	for(int i = 0; i < data.size() - 1; i++)
-	{
-		for (int j = i + 1; j < data.size(); j++)
-		{
-			numComp++;
-			if (data[i].ubiStr > data[j].ubiStr)
-			{
-				std::swap(data[i], data[j]);
-			}
-			else if (data[i].ubiStr == data[j].ubiStr)
-			{
-				if (data[i].fechaInt > data[j].fechaInt)
-				{
-					std::swap(data[i], data[j]);
-				}
-			}
-		}
-	}
-}
-
-string ubiToStart(string ubi)
-{
-	string ubiStart;
-	for (int i = 0; i < 3; i++)
-	{
-		ubiStart += ubi[i];
-	}
-	return ubiStart;
-}
+#include "DateConverter.h"
+#include "Functions.h"
 
 int main()
 {
